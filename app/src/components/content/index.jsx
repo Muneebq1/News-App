2c3264f596548356fe296e9e34d67134c9df2f4a
muneeb <muneebshahzad244@gmail.com>
2022-11-16 07:32:33 -0800

    styling

--- app/src/components/content/index.jsx
+++ app/src/components/content/index.jsx
@@ -1,10 +1,10 @@
 import './index.css'
 import axios from "axios"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import moment from "moment/moment";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrophy , faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTrophy, faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Content = () => {
 
@@ -12,8 +12,32 @@ const Content = () => {
     const [querry, SetQuerry] = useState("");
     const [isLoading, SetisLoading] = useState(false);
 
-    // function getTrendingNews()
+    useEffect(() => {
+        function getTrendingNews() {
 
+            const options = {
+                method: 'GET',
+                url: 'https://bing-news-search1.p.rapidapi.com/news',
+                params: { safeSearch: 'Off', textFormat: 'Raw' },
+                headers: {
+                    'X-BingApis-SDK': 'true',
+                    'X-RapidAPI-Key': '5a036c0c2cmsh5fbf592e1385f18p1132e4jsne1e1cece2046',
+                    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+                }
+            };
+
+            axios.request(options).then(function (response) {
+
+                SetData(response.data.value)
+
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+
+        getTrendingNews();
+
+    }, [])
 
     const getNews = (e) => {
         e.preventDefault();
@@ -42,58 +66,52 @@ const Content = () => {
     }
     return (
 
-        <div>
+        <div >
 
             <form onSubmit={getNews} >
                 <div className='flex'>
                     <img className='micro' src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png" alt="logo" />
                     <p className="logo">Microsoft Bing</p>
-
-
                     <input type="text"
-                        className='input'
-
-
-                        onChange={(e) => {
-
-                            SetQuerry
-
-                                (e.target.value)
-                        }
-                        }
-                    />
+                        className='input' onChange={(e) => {
+                            SetQuerry(e.target.value)
+                        }} />
                     <button className='sign-in'>Sign in</button>
                     <p className='reward'>Rewards</p>
-                    <FontAwesomeIcon className='icon , trophy' icon = {faTrophy} />
-                    <FontAwesomeIcon className='icon' icon = {faBars} />
+                    <FontAwesomeIcon className='icon , trophy' icon={faTrophy} />
+                    <FontAwesomeIcon className='icon' icon={faBars} />
 
-                   
+
                 </div>
             </form>
 
-            <div >
+            <div className='main'>
+
+                <h1>Trending News</h1>
+
                 {(isLoading) ? "loading.." : ""}
 
-                {Data.map(eachPost => (
 
-                    <div key={eachPost?.name}>
+                    {Data.map(eachPost => (
 
-                        <a href={eachPost?.url}>{eachPost?.name}</a>
+                        <div key={eachPost?.name} className="post">
+                            <a href={eachPost?.url}>{eachPost?.name}</a>
+                            <span>{
+                                moment(eachPost?.datePublished).format('MMMM Do YYYY , h:mm a')
+                            }
+                            </span>
+                            <h3>{eachPost?.description}</h3>
+                            <img src={eachPost?.image?.thumbnail?.contentUrl
+                                .replace("&pid=News", "")
+                                .replace("pid=News&", "")
+                                .replace("pid=News", "")
+                            }
+                                alt="" />
 
-                        <span>{
-                            moment(eachPost?.datePublished).format('MMMM Do YYYY , h:mm a')
-                        }
-                        </span>
-                        <h3>{eachPost?.description}</h3>
-                        <img src={eachPost?.image?.thumbnail?.contentUrl
-                            .replace("&pid=News", "")
-                            .replace("pid=News&", "")
-                            .replace("pid=News", "")
-                        }
-                            alt="" />
+                        </div>
 
-                    </div>
-                ))}
+                    ))}
+                
 
             </div>
         </div>
